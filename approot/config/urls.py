a52662c67d141f0fd7faafde4b157448ec84c0a6
pkozhem <pkozhem@gmail.com>
2022-10-07 18:27:52 +0300

    Initial commit with already configured settings.

---
+++ approot/config/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('api/v1/', include('src.routers')),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
