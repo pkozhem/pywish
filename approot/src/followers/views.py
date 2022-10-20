fbffed9482d55a432741997b42a56e9807491f80
pkozhem <pkozhem@gmail.com>
2022-10-20 04:33:37 +0300

    Fixed bug: now it's unavailable to start following a single person more than one time. Unfollow works properly.

--- approot/src/followers/views.py
+++ approot/src/followers/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.views import APIView
 from src.followers.models import Follower
 from src.followers.serializers import FollowerSerializer
 
@@ -29,10 +29,17 @@ class FollowerAPIView(APIView):
 
     def post(self, request, pk):
         user = User.objects.get(id=self.kwargs['pk'])
+
+        if User.objects.filter(followers__user=user, followers__follower=request.user):
+            return Response(data={"details": "You are already following this user."},
+                            status=status.HTTP_204_NO_CONTENT)
+
         Follower.objects.create(user=user, follower=request.user)
-        return Response(data={"details": f"{request.user} started follow {user}"}, status=status.HTTP_201_CREATED)
+        return Response(data={"details": f"{request.user} started follow {user}."},
+                        status=status.HTTP_201_CREATED)
 
     def delete(self, request, pk):
         user = User.objects.get(id=self.kwargs['pk'])
         Follower.objects.get(user=user, follower=request.user).delete()
-        return Response(data={"details": f"{request.user} no more follows {user}"}, status=status.HTTP_204_NO_CONTENT)
+        return Response(data={"details": f"{request.user} no more follows {user}."},
+                        status=status.HTTP_204_NO_CONTENT)
