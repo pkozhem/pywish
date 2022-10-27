aaa4acc0237437eb670c8126682b0bd9574200bf
pkozhem <pkozhem@gmail.com>
2022-10-28 02:15:18 +0300

    Created controller for Wishes.

--- approot/src/users/urls.py
+++ approot/src/users/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from src.users.views import UserPrivateAPIView, UserPublicAPIView
+from src.wishes.views import WishAPIView
 
 urlpatterns = [
-    path('<int:pk>', UserPublicAPIView.as_view({'get': 'retrieve'})),
-    path('<int:pk>/profile', UserPrivateAPIView.as_view({'get': 'retrieve',
-                                                         'put': 'update'})),
-    # path('<int:pk>/wishes', WishUpdateAPIView.as_view())
+    path('<int:pk>', UserPublicAPIView.as_view({
+        'get': 'retrieve'
+    })),
+
+    path('<int:pk>/profile', UserPrivateAPIView.as_view({
+        'get': 'retrieve',
+        'put': 'update'
+    })),
+
+    path('<int:pk>/wishes', WishAPIView.as_view({
+            'get': 'retrieve',
+            'put': 'update'
+        }))
 ]
--- approot/src/wishes/views.py
+++ approot/src/wishes/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import AllowAny
+from src.wishes.models import Wish
+from src.wishes.serializers import WishSerializer
+from src.tools.views import RetrieveUpdateGenericViewSet
+from src.tools.permissions import IsOwnerOrAdmin
 
-# Create your views here.
+User = get_user_model()
+
+
+class WishAPIView(RetrieveUpdateGenericViewSet):
+    permission_classes = [AllowAny]
+    serializer_class = WishSerializer
+
+    def get_queryset(self):
+        user = User.objects.get(id=self.kwargs['pk'])
+        return Wish.objects.filter(user=user)
+
+    def get_permissions(self):
+        if self.action == 'get':
+            self.permission_classes = [AllowAny]
+        elif self.action == 'update':
+            self.permission_classes = [IsOwnerOrAdmin]
+        return super(self.__class__, self).get_permissions()
