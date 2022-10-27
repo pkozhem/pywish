ae93190cd2c0a0f305bc173a81352ee511a15303
pkozhem <pkozhem@gmail.com>
2022-10-27 18:10:18 +0300

    Auto create Wish instance when User is created.

--- approot/src/users/signals.py
+++ approot/src/users/signals.py
@@ -1,21 +1,22 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
-from src.users.models import Profile
+from src.users.models import Profile, Wish
 
 User = get_user_model()
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
+def create_profile_and_wish(sender, instance, created, **kwargs):
     """ Function which creates User's Profile. """
 
     if created:
         Profile.objects.create(user=instance)
+        Wish.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, **kwargs):
+def create_profile_and_wish(sender, instance, **kwargs):
     """ Function which saves modified data. """
 
     instance.profile.save()
